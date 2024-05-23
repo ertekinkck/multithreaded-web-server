af58619f927bcade98307e7bccdffd2e19d5a608
ertekinkck <ertekinkck@gmail.com>
2024-05-23 23:55:15 +0300

    Server thread update, gives segfault

--- server.c
+++ server.c
@@ -13,21 +13,22 @@
 
 typedef struct {
     int connfd;
-    char filename[MAXLINE];
+    //char filename[MAXLINE];
     struct timespec mod_time;
-    time_t send_time;
     off_t file_size;
 } request_t;
 
 // Initializing Mutex Lock in main function
 pthread_mutex_t mutex;
 
-sem_t empty, full, thread_mutex;
+sem_t empty, full;
 
 int buffer_size=0;
+int* num_threads ;
 request_t *buffer = NULL;
-
-int buffer_index = 0;
+pthread_t *thread_pool;
+int buffer_start = 0;
+int buffer_end = 0;
 char *sched_policy;
 
 void getargs(int *port, int *num_threads, int *buffer_size, char **sched_policy, int argc, char *argv[]) {
@@ -47,41 +48,30 @@ void getargs(int *port, int *num_threads, int *buffer_size, char **sched_policy,
 void push_to_buffer(request_t* req) {
     sem_wait(&empty);
     pthread_mutex_lock(&mutex);
-
-    // Check if the buffer is full
-    if (buffer_index >= buffer_size) {
-        // Buffer is full, handle the error (e.g., drop the request or wait)
-        fprintf(stderr, "Error: Buffer is full. Dropping the request...\n");
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
-        free(req);
-        return;
-    }
     
-    buffer[buffer_index++] = *req;
+    buffer[buffer_end] = *req;
+    buffer_end = (buffer_end + 1) % buffer_size;
     
     pthread_mutex_unlock(&mutex);
     sem_post(&full);
 
-    printf("Pushed to buffer: %s\n", req->filename); // Debugging statement
+    //printf("Pushed to buffer: %s\n", req->filename); // Debugging statement
 }
 
 request_t *pop_from_buffer() {
-    sem_wait(&full);
-    pthread_mutex_lock(&mutex);
+    
 
     request_t *req = malloc(sizeof(request_t)); // Allocate memory for req
 
     int index = -1;
 
-
     if (strcmp(sched_policy, "FIFO") == 0) { // FIFO
-        index = buffer_index;
-        buffer_index++;
+        index = buffer_start;
+        buffer_start = (buffer_start + 1) % buffer_size;
     } else if (strcmp(sched_policy, "RFF") == 0) { // RFF
         struct timespec latest_mod_time = {0, 0};
         for (int i = 0; i < buffer_size; i++) {
-            int idx = buffer_index+i;
+            int idx = (buffer_start + i) % buffer_size;
             if (buffer[idx].connfd != -1 && 
                 (latest_mod_time.tv_sec < buffer[idx].mod_time.tv_sec || 
                  (latest_mod_time.tv_sec == buffer[idx].mod_time.tv_sec && 
@@ -93,7 +83,7 @@ request_t *pop_from_buffer() {
     } else if (strcmp(sched_policy, "SFF") == 0) { // SFF
         off_t smallest_size = __LONG_MAX__;
         for (int i = 0; i < buffer_size; i++) {
-            int idx = buffer_index+i;
+            int idx = (buffer_start + i) % buffer_size;
             if (buffer[idx].connfd != -1 && buffer[idx].file_size < smallest_size) {
                 smallest_size = buffer[idx].file_size;
                 index = idx;
@@ -102,6 +92,9 @@ request_t *pop_from_buffer() {
     }
 
     if (index != -1) {
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+
         *req = buffer[index];
         buffer[index].connfd = -1; // Mark as handled
     }
@@ -109,20 +102,30 @@ request_t *pop_from_buffer() {
     pthread_mutex_unlock(&mutex);
     sem_post(&empty);
 
-    printf("Popped from buffer: %s\n", req->filename); // Debugging statement
+    //printf("Popped from buffer: %s\n", req->filename); // Debugging statement
 
     return req;
 }
 
-
-
 void *client_handler(void *arg) {
+    
     while (1) {
         request_t *req = pop_from_buffer();
-        printf("Handling request: %s\n", req->filename); // Debugging statement
+        //printf("Handling request: %s\n", req->filename); // Debugging statement
         printf("Handling request: %d\n", req->connfd); // Debugging statement
 
-        requestHandle(req->connfd);
+        for (int i = 0; i < (*num_threads); i++) {
+            if (pthread_create(&thread_pool[i], NULL, &requestHandle, (void*)&req->connfd) != 0) {
+                fprintf(stderr, "Error: pthread_create failed\n");
+                exit(1);
+            }else{
+                pthread_detach(thread_pool[i]);
+            }
+        }
+
+        //requestHandle(req->connfd);
+        //Close(req->connfd);
+        free(req);
     }
     return NULL;
 }
@@ -153,50 +156,40 @@ void handle_client(int connfd) {
     // Convert the modification time to a readable format
     tm_info = localtime(&file_stat.st_mtime);
     strftime(mod_time_str, sizeof(mod_time_str), "%H:%M:%S", tm_info);
-    
 
     request_t *req = malloc(sizeof(request_t));
     if (req == NULL) {
         fprintf(stderr, "Error: malloc failed\n");
-        Close(connfd);
+        Close(connfd); // Close the connection if memory allocation fails
         return;
     }
 
     req->connfd = connfd;
     printf("\nCONNFD: %d\n", connfd);
-    strncpy(req->filename, filename, MAXLINE);
+    //strncpy(req->filename, filename, MAXLINE);
     req->mod_time = file_stat.st_mtim;
     req->file_size = file_stat.st_size;
-
-    time_t raw_time;
-    struct tm *local_time;
-    char send_time_str[64]; // Buffer to store the formatted time
-    time(&raw_time); // Get current time
-    local_time = localtime(&raw_time);
-    req->send_time = raw_time;
-    strftime(send_time_str, sizeof(send_time_str), "%H:%M:%S", local_time);
-
-
-    printf("Received request: %s (size: %ld, modified: %s, sending time: %s)\n", filename, file_stat.st_size, mod_time_str, send_time_str);
+    
+    printf("Received request: %s (size: %ld, modified: %s)\n", filename, file_stat.st_size, mod_time_str);
     push_to_buffer(req);
 }
 
 int main(int argc, char *argv[]) {
-    int port, num_threads;
-    pthread_t *thread_pool;
+    int port;
+    
     struct sockaddr_in clientaddr;
     socklen_t clientlen;
 
-    getargs(&port, &num_threads, &buffer_size, &sched_policy, argc, argv);
-
+    getargs(&port, num_threads, &buffer_size, &sched_policy, argc, argv);
+    
     // initialize mutex lock
     pthread_mutex_init(&mutex, NULL);
-    sem_init(&empty, 0, buffer_size);
+    sem_init(&empty, 0, (*num_threads));
     sem_init(&full, 0, 0);
-    sem_init(&thread_mutex, 0, num_threads);
 
+    thread_pool = (pthread_t *)malloc(sizeof(pthread_t) * (*num_threads));
     buffer = (request_t*)malloc(sizeof(request_t) * (buffer_size));
-    thread_pool = (pthread_t *)malloc(sizeof(pthread_t) * num_threads);
+    
 
     if (buffer == NULL) {
         fprintf(stderr, "Error: buffer malloc failed\n");
@@ -206,19 +199,17 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Error: malloc failed\n");
         exit(1);
     }
-
-    for (int i = 0; i < num_threads; i++) {
-        sem_wait(&thread_mutex);
-        if (pthread_create(&thread_pool[i], NULL, client_handler, NULL) != 0) {
-            fprintf(stderr, "Error: pthread_create failed\n");
-            exit(1);
-        }
-    }
+    pthread_t main_thread;
+    if (pthread_create(&main_thread, NULL, &client_handler, NULL) == 0){
+        pthread_detach(main_thread);
+    } 
+    else exit(1);
+    
 
     int listenfd = Open_listenfd(port);
     while (1) {
         clientlen = sizeof(clientaddr);
-        int connfd = Accept(listenfd, (SA *)&clientaddr, (socklen_t *)&clientlen);
+        int connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
         printf("Accepted connection from client\n"); // Debugging statement
         handle_client(connfd);
     }
@@ -228,7 +219,6 @@ int main(int argc, char *argv[]) {
     pthread_mutex_destroy(&mutex);
     sem_destroy(&empty);
     sem_destroy(&full);
-    sem_destroy(&thread_mutex);
 
     return 0;
 }
